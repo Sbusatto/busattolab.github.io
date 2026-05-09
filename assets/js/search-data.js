dc469c29d90d62551432b2280e22d847f0c5d469
Sbusatto <s.busatto.89@gmail.com>
2026-05-09 19:04:18 +0000

    Deploying to gh-pages from @ Sbusatto/busattolab.github.io@1bfcdee6392e126e5120900ad8b61e1d95ce3f53 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/patents/";
           },
-        },{id: "nav-research",
-          title: "research",
-          description: "Our lab investigates [1-sentence summary of your niche]. We are currently focused on the following pillars.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-team",
           title: "team",
           description: "Meet the researchers at the Biological Nanoparticles Laboratory.",
